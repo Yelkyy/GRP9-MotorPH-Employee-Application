b01fce74266921da9aedb08958ba0c83c0a9a20f
Yelkyy <lr.jmpoblete@mmdc.mcl.edu.ph>
2025-05-15 14:22:31 +0800

    Initial cleanup: Removed unused Java classes, updated login UI

---
+++ src/motorph/ui/main/Login.java
@@ -0,0 +1,15 @@
+package motorph.ui.main;
+
+import motorph.ui.LoginFrame;
+
+public class Login {
+    
+    public static void main(String[] args){
+        LoginFrame LoginF = new LoginFrame();
+        LoginF.setVisible(true);
+        LoginF.pack();
+        LoginF.setLocationRelativeTo(null);
+    }
+    
+    
+}
